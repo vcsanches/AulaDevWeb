3cc90f1d07172133d8f05e7e486cf44bcea23240
“vcsanches” <11911611@animaedu.intranet>
2019-10-04 20:29:00 -0300

    Primeiro commit

---
+++ Index.php
@@ -0,0 +1,21 @@
+<?php
+define("IP", "127.0.0.1");
+echo IP;
+echo "<br>";
+echo "<br>";
+
+$numero = 1;
+	echo $numero;
+
+echo "<br>";
+echo "<br>";
+
+echo "Hello World";
+
+echo "<br>";
+echo "<br>";
+
+$num1 = 1;
+$num2 = 2;
+
+echo("O resultado da soma é: ".($num1 + $num2));
