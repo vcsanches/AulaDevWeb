d8434c75fd1f8dad9017d0dc341c8f0074c20b23
“vcsanches” <11911611@animaedu.intranet>
2019-10-04 22:37:09 -0300

    Index2

--- Index2.php
+++ Index2.php
@@ -3,6 +3,9 @@
 class Pessoa {
 
 	private $altura = "1.67";
+	private $olhos = "Castanhos";
+	private $Cabelo = "Castanho escuro";
+	public $CPF;
 	
 	public function setAltura($altura){
 		
@@ -13,12 +16,32 @@ class Pessoa {
 		
 		return $this->altura ;
 	}
+	
+	public function setOlhos($olhos){
 		
+	$this->olhos = $olhos;
+	}
+	
+	public function getolhos(){
+		
+		return $this->olhos ;
+	}
+	
+	
+	
+	
+	
+	public function __construct($altura, $olhos){
+		$this->setAltura($altura);
+		$this->setOlhos($olhos);
+	}
 }
 
 
 
-$obj = new Pessoa;
+$obj = new Pessoa("1.80", "Azuis");
+
+
 
 var_dump($obj);
 
@@ -26,11 +49,29 @@ echo "<br>";
 echo "<br>";
 
 echo $obj->getAltura() * 2;
-
+echo "<br>";
+echo "<br>";
 
 $obj->setAltura("2.00");
 
 echo "<br>";
 echo "<br>";
 
-echo $obj->getAltura();
+
+echo $obj->getOlhos();
+echo "<br>";
+echo "<br>";
+
+$obj2 = new Pessoa("1.60", "Pretos");
+
+	
+echo $obj2->getOlhos();
+echo "<br>";
+echo "<br>";
+
+$obj2->setOlhos("Olhos castanhos");
+
+
+echo $obj2->getOlhos();
+echo "<br>";
+echo "<br>";
